61e235cad3aef766b4da6576bee51606edef5cea
Max <sultonbek.rakhimov@gmail.com>
2023-10-20 11:41:10 +0500

    Finish producer example

---
+++ src/AsyncComms.RabbitMq.CourseRabbitInNet.N1.Producer/Program.cs
@@ -0,0 +1,6 @@
+﻿using AsyncComms.RabbitMq.CourseRabbitInNet.N1.Producer;
+
+var sender = new Sender();
+sender.Publish();
+
+Console.ReadLine();
---
+++ src/AsyncComms.RabbitMq.CourseRabbitInNet.N1.Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace AsyncComms.RabbitMq.CourseRabbitInNet.N1.Producer;
+
+public class Sender
+{
+    public void Publish()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare("BasicTest", false, false, false);
+
+        var message = "Getting started with .NET Core Rabbit MQ";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "BasicTest", null, body);
+    }
+}
