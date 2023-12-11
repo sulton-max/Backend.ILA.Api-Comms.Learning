f00412f48134b42dae867cf06946de979699dacb
Max <sultonbek.rakhimov@gmail.com>
2023-12-11 12:54:27 +0500

    Finish jump start producer - consumer example

---
+++ src/JumpStartCs.Starter.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// create connection factory 
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+// create connection
+var connection = await connectionFactory.CreateConnectionAsync();
+
+// create channel
+var channel = await connection.CreateChannelAsync();
+
+// declare queue
+await channel.QueueDeclareAsync(
+    queue: "test-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+// create event consumer
+var consumer = new EventingBasicConsumer(channel);
+
+// add event handler and processor
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    // invoke processor
+
+    Console.WriteLine($"Message received: {message}");
+};
+
+// subscribe to queue
+await channel.BasicConsumeAsync(queue: "test-queue", autoAck: true, consumer: consumer);
+
+Console.ReadLine();
---
+++ src/JumpStartCs.Starter.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// create connection factory
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+// create connection 
+var connection = await connectionFactory.CreateConnectionAsync();
+
+// create channel
+var channel = await connection.CreateChannelAsync();
+
+// declare queue
+await channel.QueueDeclareAsync(
+    queue: "test-queue", 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null);
+
+// produce message
+var message = "Hello Async Communication!";
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+// publish message
+await channel.BasicPublishAsync("", "test-queue", messageBody);
