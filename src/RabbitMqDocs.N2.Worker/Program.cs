b25e91f6c879aec851fb441cf37de1e256f8a365
Max <sultonbek.rakhimov@gmail.com>
2023-12-11 12:53:28 +0500

    Finish docs round-robin and fair dispatch examples

---
+++ src/RabbitMqDocs.N2.Worker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// create connection factory
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+// create connection
+using var connection = await factory.CreateConnectionAsync();
+
+// create channel
+using var channel = await connection.CreateChannelAsync();
+
+// set prefetch count
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+// declare queue
+channel.QueueDeclare(queue: "hello-messaging", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+// create consumer
+var consumer = new EventingBasicConsumer(channel);
+
+// create event handler
+consumer.Received += async (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    await Task.Delay(5_000);
+
+    Console.WriteLine($"Received message: {message}");
+    
+    await channel.BasicAckAsync(deliveryTag: args.DeliveryTag, multiple: false);
+};
+
+// start consuming
+channel.BasicConsume(queue: "hello-messaging", autoAck: false, consumer: consumer);
+
+Console.ReadLine();
---
+++ src/RabbitMqDocs.N3.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var renderProcessingQueueName = "RenderProcessing";
+
+// create connection factory
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+// create connection
+var connection = await connectionFactory.CreateConnectionAsync();
+
+// create channel
+var channel = await connection.CreateChannelAsync();
+
+// create consumer
+var consumer = new EventingBasicConsumer(channel);
+
+// handle received message
+consumer.Received += async (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    // process message
+    Console.WriteLine($"Message received: {message}");
+
+    // acknowledge message
+    await channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+// start consuming
+channel.BasicConsume(queue: renderProcessingQueueName, autoAck: false, consumer: consumer);
+
+Console.ReadLine();
