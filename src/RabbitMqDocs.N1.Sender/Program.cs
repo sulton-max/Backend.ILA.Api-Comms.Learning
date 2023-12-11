fb560ea78fb84a2ee81e3b4b1edfa681909b5756
Max <sultonbek.rakhimov@gmail.com>
2023-12-11 12:53:08 +0500

    Finish docs sender example

---
+++ src/RabbitMqDocs.N1.Sender/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// create connection factory
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+// create connection
+using var connection = await factory.CreateConnectionAsync();
+
+// create channel
+using var channel = await connection.CreateChannelAsync();
+
+// declare queue
+await channel.QueueDeclareAsync("hello-world", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+// produce message
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello-world", body: body);
