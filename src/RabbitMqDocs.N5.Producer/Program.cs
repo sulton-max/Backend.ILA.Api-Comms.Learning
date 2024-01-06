ace1e2e1f72c78211650ba4c622b1a7e3f10a480
Max <sultonbek.rakhimov@gmail.com>
2024-01-06 10:33:16 +0500

    Update N2 and N5 examples

---
+++ src/RabbitMqDocs.N5.Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// create connection factory
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+// connection - RabbitMqConnectionProvider - CreateChannel - 
+
+// ResumeGeneratorService - ResumeProcessingSubscriber
+// NotificationAggregatorService - NotificationProcessingSubscriber
+
+// create connectioan
+var connection = await connectionFactory.CreateConnectionAsync();
+
+// create channel
+var channel = await connection.CreateChannelAsync();
+
+// create queue
+await channel.QueueDeclareAsync(
+    queue: "hello-messaging", 
+    durable: true, 
+    exclusive: false, 
+    autoDelete: false);
+
+// produce message
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+// publish message
+await channel.BasicPublishAsync(
+    exchange: string.Empty, 
+    routingKey: "hello-messaging", 
+    body: body);
